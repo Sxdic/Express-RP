be4061c177bf7c2a433de61bb2b78d9c04d2b655
Sxdic <70368207+Sxdic@users.noreply.github.com>
2020-08-28 12:57:30 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client(),
+    config = require('./config.json'),
+    fs = require('fs')
+ 
+client.login(process.env.token)
+client.commands = new Discord.Collection()
+ 
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    command.run(message, args, client)
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    if (command.guildOnly && !message.guild) return message.channel.send('Cette commande ne peut être utilisée que dans un serveur.')
+    command.run(message, args, client)
+})
+
+client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`**${member} a rejoint le serveur. Nous sommes désormais ${member.guild.memberCount} ! 🎉**`)
+    member.roles.add(config.greeting.role)
+})
+ 
+client.on('guildMemberRemove', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`**${member.user.tag} a quitté le serveur... 😢**`)
+})
+
+client.on('ready', () => {
+    const statuses = [
+        '🌐 IP : Soon...',
+        '💸 Free-Access ❌',
+        '🎉 e!help ⭐'
+    ]
+    let i = 0
+    setInterval(() => {
+        client.user.setActivity(statuses[i], {type: 'PLAYING'})
+        i = ++i % statuses.length
+    }, 1e4)
+})
